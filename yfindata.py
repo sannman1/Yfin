d06f65071b47a2cd904e978c1118c179f72cfeb8
Sannman Prabhu <prabhusanman2@gmail.com>
2025-06-19 21:45:03 +0530

    made some changes to how yfin process data

--- yfindata.py
+++ yfindata.py
@@ -1,17 +1,17 @@
 import yfinance as yf
 import pandas as pd
-import datetime
 
-tickers = ["RELIANCE.NS", "TATAMOTORS.NS"]
-
-end = datetime.datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
-start = end - datetime.timedelta(days=30)
-
-for symbol in tickers:
-    df = yf.Ticker(symbol).history(start=start, end=end, interval='1d')
-    print(f"\n{symbol} data:\n", df)
+def get_stock_data(tickers=None, period="30d"):
+    if tickers is None:
+        tickers = ["RELIANCE.NS","TCS.NS"]
     
-    csv_filename = f"{symbol.replace('.NS', '')}_price_data.csv"
-    df.to_csv(csv_filename, index=True)  # keep index (date)
-    print(f"Saved to {csv_filename}")
+    data = yf.download(tickers, period=period, interval="1d", group_by="ticker")
 
+    # Flatten the DataFrame
+    flat_df = (
+        data.stack(level=1)
+        .reset_index()
+        .rename(columns={"level_1": "Ticker"})
+    )
+    
+    return flat_df
