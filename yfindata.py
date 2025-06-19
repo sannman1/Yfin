d8deab33f8d91a4fdbed0272d36bffd116222b49
Sannman Prabhu <prabhusanman2@gmail.com>
2025-06-19 23:53:05 +0530

    Update yfindata.py
    changed data display method

--- yfindata.py
+++ yfindata.py
@@ -1,17 +1,24 @@
 import yfinance as yf
 import pandas as pd
 
-def get_stock_data(tickers=None, period="30d"):
+def get_stock_data(tickers=None, period="5y"):
     if tickers is None:
-        tickers = ["RELIANCE.NS","TCS.NS"]
-    
-    data = yf.download(tickers, period=period, interval="1d", group_by="ticker")
+        tickers = ["RELIANCE.NS", "TCS.NS"]
 
-    # Flatten the DataFrame
-    flat_df = (
-        data.stack(level=1)
-        .reset_index()
-        .rename(columns={"level_1": "Ticker"})
-    )
-    
-    return flat_df
+    data = yf.download(tickers, period=period, interval="1d", group_by='ticker', auto_adjust=False)
+
+    # If single ticker, convert to multi-index manually
+    if not isinstance(data.columns, pd.MultiIndex):
+        # data.columns: Index(['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'], dtype='object')
+        data.columns = pd.MultiIndex.from_product([data.columns, tickers])
+
+    frames = []
+    for ticker in tickers:
+        df = data[ticker].copy()
+        df["Date"] = df.index
+        df["Ticker"] = ticker
+        frames.append(df.reset_index(drop=True))
+
+    final_df = pd.concat(frames)
+    final_df = final_df[["Date", "Ticker", "Open", "High", "Low", "Close", "Volume"]]
+    return final_df
